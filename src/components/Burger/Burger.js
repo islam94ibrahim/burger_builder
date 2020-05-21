910ba40011ba1b334663bc68397e4e1b3ffa2ceb
Islam Ibrahim <islam94.ibrahim@gmail.com>
2020-05-21 18:00:41 +0200

    feat: add burger ingerdients state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './Burger.module.css';
 import BurgerIngerdient from './BurgerIngerdient/BurgerIngerdient';
 
 const Burger = (props) => {
+  let transformedIngerdients = Object.keys(props.ingerdients)
+    .map((ingerdient) => {
+      return [...Array(props.ingerdients[ingerdient])].map((_, index) => (
+        <BurgerIngerdient key={ingerdient + index} type={ingerdient} />
+      ));
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngerdients.length === 0) {
+    transformedIngerdients = <p>Please start adding ingerdients!</p>;
+  }
+
   return (
     <div className={styles.Burger}>
       <BurgerIngerdient type="bread-top" />
-      <BurgerIngerdient type="cheese" />
-      <BurgerIngerdient type="bacon" />
-      <BurgerIngerdient type="meat" />
+      {transformedIngerdients}
       <BurgerIngerdient type="bread-bottom" />
     </div>
   );
+};
+
+Burger.prototype = {
+  ingerdients: PropTypes.object.isRequired,
 };
 
 export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingerdients: {
+      salad: 0,
+      cheese: 0,
+      bacon: 0,
+      meat: 0,
+    },
+  };
   render() {
     return (
       <Fragment>
-        <Burger />
+        <Burger ingerdients={this.state.ingerdients} />
         <div>Controls</div>
       </Fragment>
     );
