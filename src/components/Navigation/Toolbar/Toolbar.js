3dcc19abe3e97b1f32997c59f839d069f637009f
Islam Ibrahim <islam94.ibrahim@gmail.com>
2020-05-23 16:57:03 +0200

    feat: add side drawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,17 @@ import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const Toolbar = (props) => (
   <header className={styles.Toolbar}>
-    <div>MENU</div>
-    <Logo />
-    <NavigationItems />
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={styles.Logo}>
+      <Logo />
+    </div>
+    <nav className={styles.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,33 @@
+import React, { Fragment, Component } from 'react';
+
+import styles from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
+
+  sideDrawerCloseHandler = () => this.setState({ showSideDrawer: false });
+
+  sideDrawerToggleHandler = () =>
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+
+  render() {
+    return (
+      <Fragment>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
+        <main className={styles.Content}>{this.props.children}</main>
+      </Fragment>
+    );
+  }
+}
+
+export default Layout;
